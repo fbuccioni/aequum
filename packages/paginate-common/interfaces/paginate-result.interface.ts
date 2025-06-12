a9c24c82a3b5391cee4658d5baac40315696bb55
Felipe Buccioni <falcacibar@gmail.com>
2025-06-12 15:09:00 -0400

    refactor(paginate-common): New pagination convention

--- packages/paginate-common/interfaces/paginate-result.interface.ts
+++ packages/paginate-common/interfaces/paginate-result.interface.ts
@@ -2,16 +2,30 @@
  * Common paginated result interface for all the
  * paginated queries
  *
- * @template Model - Model type to be paginated
+ * @typeParam Model - Model type to be paginated
  */
-export interface PaginateResult<Model> {
+export interface PaginateResult<Model, Paginator = PaginateResultPaginator> {
+    /** Paginated data requested */
     data: Model[];
-    paginator: {
-        page: number;
-        size: number;
-        pages: number;
-        next: number | null;
-        prev: number | null;
-        total: number;
-    }
+    /** Paginator info */
+    paginator: Paginator;
+}
+
+/**
+ * Common paginated result paginator data
+ * interface
+ */
+export interface PaginateResultPaginator {
+    /** Current page */
+    page: number;
+    /** Per page Size */
+    size: number;
+    /** Total pages */
+    pages: number;
+    /** Next page */
+    next: number | null;
+    /** Prev page */
+    prev: number | null;
+    /** Total items */
+    total: number;
 }
